12419d35842e286263f40ec4010f561ad121f1c7
Y-Baker <yuossefahmedgaber@gmail.com>
2024-11-27 15:36:21 +0200

    update-patient Attributes

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Medical.Models;
 
 public class AppUser : IdentityUser
 {
+    [StringLength(256)]
+    [Required]
+    public string? Name { get; set; }
+
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
